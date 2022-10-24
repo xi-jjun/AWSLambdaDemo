9fa6df243703985912cf643a9d1f3d42693bfd16
xi_jjun <rlawowns97@naver.com>
2022-10-24 20:30:25 +0900

    refactor: Serialization 위해서 StreamHandler가 아닌, Generic type을 받을 수 있게 변경

--- src/main/java/me/practice/awslambdademo/LambdaHandler.java
+++ src/main/java/me/practice/awslambdademo/LambdaHandler.java
@@ -1,7 +1,9 @@
 package me.practice.awslambdademo;
 
-import org.springframework.cloud.function.adapter.aws.SpringBootStreamHandler;
+import me.practice.awslambdademo.domain.OrderRequestDto;
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
 
-public class LambdaHandler extends SpringBootStreamHandler {
+// 여기 클래스의 Generic 이 바로 Serialize 해주는 역할
+public class LambdaHandler extends SpringBootRequestHandler<OrderRequestDto, Object> {
 
 }
