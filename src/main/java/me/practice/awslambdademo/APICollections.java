7f1ecc63a4bda3acaf4ec4ad7a85035a2c437958
xi_jjun <rlawowns97@naver.com>
2022-10-25 20:41:46 +0900

    feat: api 추가

--- src/main/java/me/practice/awslambdademo/APICollections.java
+++ src/main/java/me/practice/awslambdademo/APICollections.java
@@ -2,18 +2,15 @@ package me.practice.awslambdademo;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import me.practice.awslambdademo.domain.Order;
+import me.practice.awslambdademo.domain.ItemRegisterDto;
 import me.practice.awslambdademo.domain.OrderRequestDto;
-import me.practice.awslambdademo.domain.OrderResponseDto;
+import me.practice.awslambdademo.domain.ResponseDto;
 import me.practice.awslambdademo.service.OrderService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,19 +19,27 @@ public class APICollections {
 	private final OrderService orderService;
 
 	@Bean
-	public Function<OrderRequestDto, OrderResponseDto> registerOrderItem() {
-		orderService.getOrders()
-				.forEach(order -> log.info(order.toString()));
+	public Function<ItemRegisterDto, ResponseDto<?>> registerItem() {
+		return orderService::registerItem;
+	}
+
+	@Bean
+	public Supplier<ResponseDto<?>> getAllItemList() {
+		return orderService::getTotalItemList;
+	}
+
+	@Bean
+	public Function<OrderRequestDto, ResponseDto<?>> orderItem() {
 		return orderService::orderItem;
 	}
 
 	@Bean
-	public Function<String, String> reverse() {
-		return (input) -> new StringBuilder(input).reverse().toString();
+	public Function<String, ResponseDto<?>> getItem() {
+		return orderService::getItem;
 	}
 
 	@Bean
-	public Supplier<List<OrderResponseDto>> getOrderList() {
-		return orderService::getOrders;
+	public Supplier<ResponseDto<?>> getTotalOrderList() {
+		return orderService::getTotalOrderList;
 	}
 }
